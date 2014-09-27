b66668f56cc8a950193dc695aed104344c0a6e0c
Erich Uher <ejuher@gmail.com>
2014-09-26 18:21:27 -0700

    end of day, need to debug regex in Entry factory method

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,12 +3,19 @@ class Entry < ActiveRecord::Base
 
 	def self.create_by_fj(fj, id)
 		og_entry = OpenGraph.new(fj.url)
+		if og_entry.images.length == 1
+			image_url = og_entry.images.first
+		else
+			# undefined method `[]' for nil:NilClass
+			image_url = /img.*src\W+(.*.png|.jpg|.jpeg|.gif)/.match(fj.summary)[1]
+		end
+
 		Entry.create!({
 			title: fj.title,
 			url: fj.url,
 			content: fj.summary,
 			summary: og_entry.description,
-			image: og_entry.images.first,
+			image: image_url,
 			published: fj.published,
 			feed_id: id
 		})
