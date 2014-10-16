f11db363d7b3569a177c644505253923c208d28e
Erich Uher <ejuher@gmail.com>
2014-10-16 10:14:54 -0700

    changed title from string to text in entry table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016165510) do
+ActiveRecord::Schema.define(version: 20141016171314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20141016165510) do
   add_index "categories", ["name"], name: "index_categories_on_name", unique: true, using: :btree
 
   create_table "entries", force: true do |t|
-    t.string   "title"
+    t.text     "title"
     t.text     "url"
     t.datetime "created_at"
     t.datetime "updated_at"
