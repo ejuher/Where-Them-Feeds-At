18a9cf08f8b7ffb4585bfbebcc2f42db308630c7
Erich Uher <ejuher@gmail.com>
2014-10-23 16:51:18 -0700

    number of favorites appears in sidebar

--- app/assets/javascripts/views/fave_count.js
+++ app/assets/javascripts/views/fave_count.js
@@ -1,10 +1,14 @@
 Bsstrss.Views.FaveCount = Backbone.View.extend({
-	template: [faves/count],
+	template: JST['faves/count'],
 	tagName: 'li',
 
 	render: function() {
-		var renderContent = this.template({ faveCount: this.faveCount });
-		this.$el.html(renderContent);
+		var that = this;
+		$.getJSON('api/fave_count', function(resp) { 
+			faves = resp;
+			var renderContent = that.template({ faveCount: faves });
+			that.$el.html(renderContent); 
+		})
 		return this;
 	},
 })
--- app/assets/javascripts/views/feeds_index.js
+++ app/assets/javascripts/views/feeds_index.js
@@ -15,26 +15,13 @@ Bsstrss.Views.FeedsIndex = Backbone.CompositeView.extend({
 	},
 
 	render: function() {
+		var faveView = new Bsstrss.Views.FaveCount();
+		this.addSubview("div#feeds", faveView);
 		this.collection.each(this.addFeed.bind(this));
 		var renderContent = this.template({ feeds: this.collection });
 		this.$el.html(renderContent);
 		this.attachSubviews();
 		return this
 	},
-
-
-
-	// increment fave
-	// how do we pass the number to the feed index? include it in the feed index jbuilder
-	  // this is kindof awkward, would have to reset how this.collection gives feeds to the view
-	// could also make the fave a view.
-	// ok. could be a fave index item. could be passed the faves index? or the faves show? 
-	// or a new route? fave_count?
-	// well, a view needs a model. a backbone model is fetched at the show route
-	// 
-
-	// the sidebar could also be a user show page
-	// 
-
 })
 		
