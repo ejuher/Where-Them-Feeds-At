fdece2866fbffe7a468e479d4c89a1d7acf8dd1f
Erich Uher <ejuher@gmail.com>
2014-11-12 17:46:01 -0800

    updated user model unit testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,27 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   it "has a valid factory" do
-  	FactoryGirl.create(:user).should be_valid
+  	expect(FactoryGirl.create(:user)).to be_valid
   end
 
   it "is invalid without a username" do 
-  	FactoryGirl.build(:user, username: nil).should_not be_valid
+  	expect(FactoryGirl.build(:user, username: nil)).to_not be_valid
   end
 
   it "is invalid without a password_digest" do
-  	FactoryGirl.build(:user, password_digest: nil).should_not be_valid
+  	expect(FactoryGirl.build(:user, password_digest: nil)).to_not be_valid
   end
 
   it "is invalid if the username is not unique" do 
   	test_user_name = Faker::Internet.user_name
   	FactoryGirl.create(:user, username: test_user_name)
-  	FactoryGirl.build(:user, username: test_user_name).should_not be_valid
+  	expect(FactoryGirl.build(:user, username: test_user_name)).to_not be_valid
+  end
+
+  it "can set and check passwords" do 
+  	user = FactoryGirl.create(:user)
+  	password = Faker::Internet.password(6)
+  	user.password = password
+  	expect(user.is_password?(password)).to be true
   end
 end
